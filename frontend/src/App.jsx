078297f7c866b2c73d35011038de16c0bae98c85
MimSad tz <1384mst@gmail.com>
2025-03-18 05:17:51 +0330

    table in chat and button finally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,49 +17,37 @@ const TableWrapper = ({ children }) => {
 
   return (
     <>
-      <div style={{ display: 'none' }}>
+      <div style={{ display: 'none' }} className='popup-view-table'>
         {children}
       </div>
 
       <div>
-        <table className='preview-table-table'>
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-          </tr>
-        </table>
-        <button className='preview-table-button' onClick={() => setModalVisible(true)}>مشاهده جدول</button>
+        <div className='preview-table-area'>
+            <table className='preview-table-table'>
+              <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+              </tr>
+              <tr>
+                <td>Alfreds</td>
+                <td>Maria</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Moctezuma</td>
+                <td>Francisco</td>
+                <td>Mexico</td>
+              </tr>
+              <tr>
+                <td>Handel</td>
+                <td>Roland</td>
+                <td>Austria</td>
+              </tr>
+
+            </table>
+          <button className='preview-table-button' onClick={() => setModalVisible(true)}>مشاهده جدول</button>
+        </div>
       </div>
 
       {modalVisible && (
